3c30319db655e55b8c396842255d01810fc09e4b
Abdulmalik Olaoye Raji <rajimalik2002@gmail.com>
2023-06-03 21:29:31 +0300

    Add registration view @ /User/NewUser

---
+++ JobSearch/Controllers/UserController.cs
@@ -0,0 +1,28 @@
+﻿using DatabaseLayer;
+using JobSearch.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobSearch.Controllers
+{
+    public class UserController : Controller
+    {
+        private jobsearchDBEntities db = new jobsearchDBEntities();
+        // GET: User
+        public ActionResult NewUser()
+        {
+            ViewBag.UserTypeID = new SelectList(db.UserTypeTables.Where(u => u.UserTypeID != 1).ToList(), "UserTypeID", "UserType", "0");
+            return View(new UserMV());
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult NewUser(UserMV userMV)
+        {
+            ViewBag.UserTypeID = new SelectList(db.UserTypeTables.Where(u => u.UserTypeID != 1).ToList(), "UserTypeID", "UserType", userMV.UserTypeID);
+            return View(new UserMV());
+        }
+    }
+}
---
+++ JobSearch/Models/UserMV.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace JobSearch.Models
+{
+    public class UserMV
+    {
+        public UserMV()
+        {
+            Company = new CompanyMV();
+        }
+        public int UserID { get; set; }
+        public int UserTypeID { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string EmailAddress { get; set; }
+        public string ContactNo { get; set; }
+        public CompanyMV Company { get; set; }
+    }
+}
