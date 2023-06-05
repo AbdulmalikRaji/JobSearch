d3bdd59d7ac5083243b45694899d614b0d76b597
Abdulmalik Olaoye Raji <rajimalik2002@gmail.com>
2023-06-06 00:15:35 +0300

    add register function

--- JobSearch/Controllers/UserController.cs
+++ JobSearch/Controllers/UserController.cs
@@ -1,6 +1,5 @@
 ﻿using DatabaseLayer;
 using JobSearch.Models;
-using LanguageExt;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -27,7 +26,7 @@ namespace JobSearch.Controllers
                 var checkuser = db.UserTables.Where(u => u.EmailAddress == userMV.EmailAddress).FirstOrDefault();
                 if (checkuser != null)
                 {
-                    ModelState.AddModelError("Email Address", "Email Address Is Already Registered");
+                    ModelState.AddModelError("EmailAddress", "Email Address Is Already Registered");
                     return View(userMV);
                 }
                 checkuser = db.UserTables.Where(u =>u.Username == userMV.Username).FirstOrDefault();
@@ -38,32 +37,73 @@ namespace JobSearch.Controllers
                 }
                 using (var trans = db.Database.BeginTransaction())
                 {
-                    var user = new UserTable();
-                user.Username = userMV.Username;
-                user.Password = userMV.Password;
-                user.EmailAddress = userMV.EmailAddress;
-                user.UserTypeID = userMV.IsProvider == true ? 2 : 3;
-                user.ContactNo = userMV.ContactNo;
-                db.UserTables.Add(user);
-                db.SaveChanges();
-
-                if (userMV.IsProvider == true)
-                {
-                    
-                        var company = new CompanyTable();
-                        company.UserID = user.UserID;
-                        company.EmailAddress = userMV.Company.EmailAddress;
-                        company.CompanyName = userMV.Company.CompanyName;
-                        company.ContactNo = userMV.Company.ContactNo;
-                        company.Logo = "~/Content/assets/img/logo/logo.png";
-                    company.Description = userMV.Company.Description;
-                        db.CompanyTables.Add(company);
+                    try
+                    {
+                        var user = new UserTable();
+                        user.Username = userMV.Username;
+                        user.Password = userMV.Password;
+                        user.EmailAddress = userMV.EmailAddress;
+                        user.UserTypeID = userMV.IsProvider == true ? 2 : 3;
+                        user.ContactNo = userMV.ContactNo;
+                        db.UserTables.Add(user);
                         db.SaveChanges();
 
+                        if (userMV.IsProvider == true)
+                        {
 
+                            var company = new CompanyTable();
+                            
+                            company.UserID = user.UserID;
+                            if (string.IsNullOrEmpty(userMV.Company.EmailAddress)) 
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.EmailAddress", "Required");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.CompanyName))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.CompanyName", "Required");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.CompanyName))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.CompanyName", "Required");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.PhoneNo))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.PhoneNo", "Required");
+                                return View(userMV);
+                            }
+                            if (string.IsNullOrEmpty(userMV.Company.Description))
+                            {
+                                trans.Rollback();
+                                ModelState.AddModelError("Company.Description", "Required");
+                                return View(userMV);
+                            }
+                            company.EmailAddress = userMV.Company.EmailAddress;
+                            company.CompanyName = userMV.Company.CompanyName;
+                            company.ContactNo = userMV.ContactNo;
+                            company.PhoneNo = userMV.Company.PhoneNo;
+                            company.Logo = "~/Content/assets/img/logo/logo.png";
+                            company.Description = userMV.Company.Description;
+                            db.CompanyTables.Add(company);
+                            db.SaveChanges();
+
+
+                        }
+                        trans.Commit();
+                        return RedirectToAction("Login");
                     }
-                    trans.Commit();
-                    return RedirectToAction("Login");
+                    catch (Exception)
+                    {
+                        ModelState.AddModelError(String.Empty, "Please Provide Correct Details");
+                        trans.Rollback();
+                    }
+                   
                 }
 
             }
